3b01ef7531c7d6bd755b4493fd34f16104f0d2ed
pokhalo <mindenhez@protonmail.com>
2024-08-22 15:21:45 +0300

    added carbon emissions prediction, responsive design

--- src/frontend/src/Form.jsx
+++ src/frontend/src/Form.jsx
@@ -76,17 +76,21 @@ export default function Form() {
   };
 
   const fetchData = async () => {
-    const url = `http://localhost:5000/forecast/biowaste_from_meals?restaurant=${values.location}&num_fish=${values.fish}&num_chicken=${values.chicken}&num_vegetarian=${values.vegetarian}&num_meat=${values.meat}&num_vegan=${values.vegan}&return_type=numeric`;
+    const urlWaste = `https://megasense-server.cs.helsinki.fi/fwowebserver/forecast/biowaste_from_meals?restaurant=${values.location}&num_fish=${values.fish}&num_chicken=${values.chicken}&num_vegetarian=${values.vegetarian}&num_meat=${values.meat}&num_vegan=${values.vegan}&return_type=numeric`;
+    const urlCarbonEmissions = `https://megasense-server.cs.helsinki.fi/fwowebserver/forecast/co2_from_meals?restaurant=${values.location}&num_fish=${values.fish}&num_chicken=${values.chicken}&num_vegetarian=${values.vegetarian}&num_meat=${values.meat}&num_vegan=${values.vegan}`
 
     try {
-      const response = await fetch(url);
-      const data = await response.json();
+      const responseWaste = await fetch(urlWaste);
+      const dataWaste = await responseWaste.json();
+      const responseCarbonEmissions = await fetch(urlCarbonEmissions);
+      const dataCarbonEmissions= await responseCarbonEmissions.json();
 
       setChartData({
-        wasteFromCustomers: [data.predicted_waste_customer],
-        wasteFromKitchen: [data.predicted_waste_kitchen],
-        wastePerCustomer: [data.predicted_waste_per_customer],
-        receipts: [data.predicted_num_receipts]
+        wasteFromCustomers: [dataWaste.predicted_waste_customer],
+        wasteFromKitchen: [dataWaste.predicted_waste_kitchen],
+        wastePerCustomer: [dataWaste.predicted_waste_per_customer],
+        receipts: [dataWaste.predicted_num_receipts],
+        carbonEmissions: [dataCarbonEmissions.predicted_co2]
       });
     } catch (error) {
       console.error("Error fetching data: ", error);
@@ -94,6 +98,7 @@ export default function Form() {
   };
 
   return (
+    
     <div>
 
       <div className="header">
@@ -104,166 +109,112 @@ export default function Form() {
         <hr class="line" />
       </div>
 
-      <form className="cardui">
-        <br />
-        <SliderWrapper>
-          chicken
+      <div className="container">
+
+        <form className="cardui">
           <br />
-          <CustomSlider
-            value={values.chicken}
-            onChange={handleInputChange("chicken")}
-            name="chicken"
-            min={0}
-            max={300}
-            aria-labelledby="chicken-slider"
-            valueLabelDisplay="auto"
-          />
-        </SliderWrapper>
+          <SliderWrapper>
+            chicken
+            <br />
+            <CustomSlider
+              value={values.chicken}
+              onChange={handleInputChange("chicken")}
+              name="chicken"
+              min={0}
+              max={300}
+              aria-labelledby="chicken-slider"
+              valueLabelDisplay="auto"
+            />
+          </SliderWrapper>
 
-        <SliderWrapper>
-          fish
-          <br />
-          <CustomSlider
-            value={values.fish}
-            onChange={handleInputChange("fish")}
-            name="fish"
-            min={0}
-            max={300}
-            aria-labelledby="fish-slider"
-            valueLabelDisplay="auto"
-          />
-        </SliderWrapper>
+          <SliderWrapper>
+            fish
+            <br />
+            <CustomSlider
+              value={values.fish}
+              onChange={handleInputChange("fish")}
+              name="fish"
+              min={0}
+              max={300}
+              aria-labelledby="fish-slider"
+              valueLabelDisplay="auto"
+            />
+          </SliderWrapper>
 
-        <SliderWrapper>
-          meat
-          <br />
-          <CustomSlider
-            value={values.meat}
-            onChange={handleInputChange("meat")}
-            name="meat"
-            min={0}
-            max={300}
-            aria-labelledby="meat-slider"
-            valueLabelDisplay="auto"
-          />
-        </SliderWrapper>
+          <SliderWrapper>
+            meat
+            <br />
+            <CustomSlider
+              value={values.meat}
+              onChange={handleInputChange("meat")}
+              name="meat"
+              min={0}
+              max={300}
+              aria-labelledby="meat-slider"
+              valueLabelDisplay="auto"
+            />
+          </SliderWrapper>
 
-        <SliderWrapper>
-          vegan
-          <br />
-          <CustomSlider
-            value={values.vegan}
-            onChange={handleInputChange("vegan")}
-            name="vegan"
-            min={0}
-            max={300}
-            aria-labelledby="vegan-slider"
-            valueLabelDisplay="auto"
-          />
-        </SliderWrapper>
+          <SliderWrapper>
+            vegan
+            <br />
+            <CustomSlider
+              value={values.vegan}
+              onChange={handleInputChange("vegan")}
+              name="vegan"
+              min={0}
+              max={300}
+              aria-labelledby="vegan-slider"
+              valueLabelDisplay="auto"
+            />
+          </SliderWrapper>
 
-        <SliderWrapper>
-          vegetarian
-          <br />
-          <CustomSlider
-            value={values.vegetarian}
-            onChange={handleInputChange("vegetarian")}
-            name="vegetarian"
-            min={0}
-            max={300}
-            aria-labelledby="vegetarian-slider"
-            valueLabelDisplay="auto"
-          />
-        </SliderWrapper>
+          <SliderWrapper>
+            vegetarian
+            <br />
+            <CustomSlider
+              value={values.vegetarian}
+              onChange={handleInputChange("vegetarian")}
+              name="vegetarian"
+              min={0}
+              max={300}
+              aria-labelledby="vegetarian-slider"
+              valueLabelDisplay="auto"
+            />
+          </SliderWrapper>
 
+          <FormControl fullWidth variant="outlined" sx={{ marginBottom: 4 }}>
+            <Select
+              id="location"
+              value={values.location}
+              onChange={handleLocationChange}
+            >
+              <MenuItem value="Chemicum"><b>Chemicum</b></MenuItem>
+              <MenuItem value="Physicum"><b>Physicum</b></MenuItem>
+              <MenuItem value="Exactum"><b>Exactum</b></MenuItem>
+            </Select>
+          </FormControl>
 
-
-        <FormControl fullWidth variant="outlined" sx={{ marginBottom: 4 }}>
-          <Select
-            id="location"
-            value={values.location}
-            onChange={handleLocationChange}
-          >
-            <MenuItem value="Chemicum"><b>Chemicum</b></MenuItem>
-            <MenuItem value="Physicum"><b>Physicum</b></MenuItem>
-            <MenuItem value="Exactum"><b>Exactum</b></MenuItem>
-          </Select>
-        </FormControl>
-
-        <div className="cardinput">
-          <Bar
-            data={{
-              labels: ['chicken', 'fish', 'meat', 'vegan', 'vegetarian'],
-              datasets: [
-                {
-                  label: "Meals",
-                  data: [
-                    values.chicken,
-                    values.fish,
-                    values.meat,
-                    values.vegan,
-                    values.vegetarian
-                  ],
-                  backgroundColor: [
-                    "#ffc048",
-                    "#ff5e57",
-                    "#a55eea",
-                    "#0be881",
-                    "#485460",
-                  ],
-                },
-              ],
-            }}
-            options={{
-              plugins: {
-                title: {
-                  display: true,
-                  text: "Input Meals",
-                },
-                legend: {
-                  display: false,
-                },
-              },
-              scales: {
-                y: {
-                  min: 0,
-                  max: 350,
-                },
-              },
-            }}
-          />
-        </div>
-
-        <Button
-          variant="contained"
-          sx={{
-            bgcolor: '#1C1C1C',
-            "&:hover": { bgcolor: '#575fcf', color: 'white' }
-          }}
-          size="large"
-          onClick={fetchData}
-        >
-          Predict
-        </Button>
-      </form>
-
-      <div className = "chartswrapper">
-
-        {chartData.wasteFromCustomers && (
-          <div className="cardwaste">
+          <div className="cardinput">
             <Bar
               data={{
-                labels: ['Customers', 'Kitchen'],
+                labels: ['chicken', 'fish', 'meat', 'vegan', 'vegetarian'],
                 datasets: [
                   {
-                    label: "Waste",
+                    label: "Meals",
                     data: [
-                      chartData.wasteFromCustomers[0],
-                      chartData.wasteFromKitchen[0]
+                      values.chicken,
+                      values.fish,
+                      values.meat,
+                      values.vegan,
+                      values.vegetarian
                     ],
                     backgroundColor: [
                       "#ffc048",
                       "#ff5e57",
+                      "#a55eea",
+                      "#0be881",
+                      "#485460",
                     ],
                   },
                 ],
@@ -272,7 +223,7 @@ export default function Form() {
                 plugins: {
                   title: {
                     display: true,
-                    text: "Waste per type (in kilograms)",
+                    text: "Input Meals",
                   },
                   legend: {
                     display: false,
@@ -281,24 +232,148 @@ export default function Form() {
                 scales: {
                   y: {
                     min: 0,
-                    max: 60,
+                    max: 350,
                   },
                 },
               }}
             />
           </div>
-        )}
 
-        {chartData.wastePerCustomer && (
-          <div className="cardwaste">
-            <Bar
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: '#1C1C1C',
+              "&:hover": { bgcolor: '#575fcf', color: 'white' }
+            }}
+            size="large"
+            onClick={fetchData}
+          >
+            Predict
+          </Button>
+        </form>
+
+        <div class="chartswrapper">
+
+          {chartData.wasteFromCustomers && (
+            <div className="cardoutput">
+              <Bar
+                data={{
+                  labels: ['Customers', 'Kitchen'],
+                  datasets: [
+                    {
+                      label: "Waste",
+                      data: [
+                        chartData.wasteFromCustomers[0],
+                        chartData.wasteFromKitchen[0]
+                      ],
+                      backgroundColor: [
+                        "#ffc048",
+                        "#ff5e57",
+                      ],
+                    },
+                  ],
+                }}
+                options={{
+                  plugins: {
+                    title: {
+                      display: true,
+                      text: "Waste per type (in kilograms)",
+                    },
+                    legend: {
+                      display: false,
+                    },
+                  },
+                  scales: {
+                    y: {
+                      min: 0,
+                      max: 60,
+                    },
+                  },
+                }}
+              />
+            </div>
+          )}
+
+          {chartData.wastePerCustomer && (
+            <div className="cardoutput">
+              <Bar
+                data={{
+                  labels: ['Waste per customer'],
+                  datasets: [
+                    {
+                      label: "Waste (g)",
+                      data: chartData.wastePerCustomer,
+                      backgroundColor: ["#a55eea"],
+                    },
+                  ],
+                }}
+                options={{
+                  plugins: {
+                    title: {
+                      display: true,
+                      text: "Waste per customer (in grams)",
+                    },
+                    legend: {
+                      display: false,
+                    },
+                  },
+                  scales: {
+                    y: {
+                      min: 0,
+                      max: 300,
+                    },
+                  },
+                }}
+              />
+            </div>
+          )}
+
+          {chartData.receipts && (
+            <div className="cardoutput">
+              <Bar
+                data={{
+                  labels: ['Receipts'],
+                  datasets: [
+                    {
+                      label: "Number of receipts",
+                      data: chartData.receipts,
+                      backgroundColor: ["#0be881"],
+                    },
+                  ],
+                }}
+                options={{
+                  plugins: {
+                    title: {
+                      display: true,
+                      text: "Number of receipts",
+                    },
+                    legend: {
+                      display: false,
+                    },
+                  },
+                  scales: {
+                    y: {
+                      min: 0,
+                      max: 800,
+                    },
+                  },
+                }}
+              />
+            </div>
+          )}
+
+          {chartData.receipts && (
+            <div className="cardoutput">
+              <Bar
               data={{
-                labels: ['Waste per customer'],
+                labels: ['Emissions'],
                 datasets: [
                   {
-                    label: "Waste (g)",
-                    data: chartData.wastePerCustomer,
-                    backgroundColor: ["#a55eea"],
+                    label: "",
+                    data: chartData.carbonEmissions,
+                    backgroundColor: [
+                      "#485460",
+                    ],
                   },
                 ],
               }}
@@ -306,7 +381,7 @@ export default function Form() {
                 plugins: {
                   title: {
                     display: true,
-                    text: "Waste per customer (in grams)",
+                    text: "Carbon emissions",
                   },
                   legend: {
                     display: false,
@@ -315,127 +390,17 @@ export default function Form() {
                 scales: {
                   y: {
                     min: 0,
-                    max: 300,
+                    max: 1000,
                   },
                 },
               }}
             />
-          </div>
-        )}
+            </div>
+          )}        
 
-        {chartData.receipts && (
-          <div className="cardwaste">
-            <Bar
-              data={{
-                labels: ['Receipts'],
-                datasets: [
-                  {
-                    label: "Number of receipts",
-                    data: chartData.receipts,
-                    backgroundColor: ["#0be881"],
-                  },
-                ],
-              }}
-              options={{
-                plugins: {
-                  title: {
-                    display: true,
-                    text: "Number of receipts",
-                  },
-                  legend: {
-                    display: false,
-                  },
-                },
-                scales: {
-                  y: {
-                    min: 0,
-                    max: 800,
-                  },
-                },
-              }}
-            />
-          </div>
-        )}
+          </div> 
 
-        {chartData.receipts && (
-          <div className="cardcarbon">
-            <Line
-            data={{
-              labels: ['1', '2', '3', '4', '5'],
-              datasets: [
-                {
-                  label: "",
-                  data: [
-                    50, 40, 120, 260, 50
-                  ],
-                  backgroundColor: [
-                    "#485460",
-                  ],
-                },
-              ],
-            }}
-            options={{
-              plugins: {
-                title: {
-                  display: true,
-                  text: "Example chart 1",
-                },
-                legend: {
-                  display: false,
-                },
-              },
-              scales: {
-                y: {
-                  min: 0,
-                  max: 350,
-                },
-              },
-            }}
-          />
-          </div>
-        )}
-
-        {chartData.receipts && (
-          <div className="cardplate">
-            <Doughnut
-            data={{
-              labels: [],
-              datasets: [
-                {
-                  label: "",
-                  data: [
-                    100, 200, 180, 200, 78
-                  ],
-                  backgroundColor: [
-                    "#ffc048",
-                    "#ff5e57",
-                    "#a55eea",
-                    "#0be881",
-                    "#485460",
-                  ],
-                },
-              ],
-            }}
-            options={{
-              plugins: {
-                title: {
-                  display: true,
-                  text: "Example chart 2",
-                },
-                legend: {
-                  display: false,
-                },
-              },
-              scales: {
-              },
-            }}
-          />
-          </div>
-        )}
-
-        
-
-        </div> 
+        </div>
 
       </div>
   );
