2ae3bb76bc5acbf27655ee0336514b02d1b98065
Hoang Le <hoangle1001@outlook.com>
2024-09-11 11:07:54 +0300

    Add code to encode dish name and dish category with LM

--- experiments_hoangle/utils.py
+++ experiments_hoangle/utils.py
@@ -4,6 +4,7 @@ from pathlib import Path
 class Paths:
     DIR_PROCESSED = Path("processed")
     DIR_RAW = Path("../src/data/basic_mvp_data")
+    DIR_RESOURCES = Path("processed/res")
 
     ##############################################################
     # Processed fact table
@@ -13,7 +14,7 @@ class Paths:
         return Paths.DIR_PROCESSED / name
 
     ##############################################################
-    # Processed fact table
+    # Processed dim tables
     ##############################################################
     @staticmethod
     def dim_lucnhes(name: str = "dim_lunches.xlsx"):
@@ -22,6 +23,17 @@ class Paths:
     @staticmethod
     def dim_biowaste(name: str = "dim_biowaste.xlsx"):
         return Paths.DIR_PROCESSED / name
+
+    ##############################################################
+    # Resources
+    ##############################################################
+    @staticmethod
+    def res_dish2embd(name: str = "map_dish2embd.npy"):
+        return Paths.DIR_RESOURCES / name
+
+    @staticmethod
+    def res_cat2embd(name: str = "map_cat2embd.npy"):
+        return Paths.DIR_RESOURCES / name
 
     ##############################################################
     # Raw data
