d3c74a92f91f187d8b41f455d6f5839e9d4739a5
Hoang Le <hoangle1001@outlook.com>
2024-09-23 13:06:03 +0300

    Add table 'dishes' and update schema of the other

--- init.sql
+++ init.sql
@@ -1,45 +1,52 @@
-CREATE TABLE IF NOT EXISTS pieces_per_dish (
-    id          SERIAL  PRIMARY KEY
-    ,date       date    NOT NULL
+
+CREATE TABLE IF NOT EXISTS dishes (
+    meal_id     INT     PRIMARY KEY
     ,restaurant TEXT    NOT NULL
-    ,category   TEXT    not null
+    ,category   TEXT    NOT NULL
     ,dish       TEXT    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pieces_per_dish (
+    meal_id     INT     NOT NULL
+    ,date       date    NOT NULL
     ,pcs        float   not null
+
+    ,constraint pk_pieces_per_dish primary key (meal_id, date)
 );
 
 
 CREATE TABLE IF NOT EXISTS pieces_whole (
-    id          SERIAL  PRIMARY KEY
     ,date       date    NOT NULL
     ,restaurant TEXT    NOT NULL
     ,pcs        float   not null
+
+    ,constraint pk_pieces_whole primary key (date, restaurant)
 );
 
 CREATE TABLE IF NOT EXISTS biowaste (
-    id          SERIAL  PRIMARY KEY
-    ,dish       TEXT    NOT NULL
+    meal_id     INT     PRIMARY KEY
     ,waste      float   not null
 );
 
 
 CREATE TABLE IF NOT EXISTS co2 (
-    id          SERIAL  PRIMARY KEY
-    ,dish       TEXT    NOT NULL
-    ,waste      float   not null
+    meal_id     INT     PRIMARY KEY
+    ,co2        float   not null
 );
 
 CREATE TABLE IF NOT EXISTS menu (
-    id                      SERIAL  PRIMARY KEY
     ,date                   DATE    NOT NULL
     ,restaurant             TEXT    not null
-    ,dish_1                 TEXT    NOT NULL
-    ,dish_2                 TEXT    NOT NULL
-    ,dish_3                 TEXT    NOT NULL
-    ,dish_4                 TEXT    NOT NULL
+    ,dish_1                 INT     NOT NULL
+    ,dish_2                 INT     NOT NULL
+    ,dish_3                 INT     NOT NULL
+    ,dish_4                 INT     NOT NULL
     ,total_co2              float   not null
     ,total_waste            float   not null
     ,total_pcs_from_dishes  float   not null
     ,co2_per_customer       float   not null
     ,waste_per_customer     float   not null
-    ,fitness                float   not null 
+    ,fitness                float   not null
+
+    ,constraint pk_menu primary key (date, restaurant)
 );
