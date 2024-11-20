1d1bb6b571b916c336b5b7fb0202a25b2509be8b
Hoang Le <hoangle1001@outlook.com>
2024-11-20 14:41:29 +0200

    Modify tables' schemma for phase 4

--- init.sql
+++ init.sql
@@ -1,18 +1,29 @@
 
-CREATE TABLE IF NOT EXISTS dishes (
-    meal_id     INT     PRIMARY KEY
-    ,restaurant TEXT    NOT NULL
-    ,category   TEXT    NOT NULL
-    ,dish       TEXT    NOT NULL
+CREATE TABLE IF NOT EXISTS meals (
+    meal_id         INT     PRIMARY KEY
+    ,meal_type_1    TEXT
+    ,meal_type_2    TEXT
+    ,restaurant     TEXT[]
+    ,schoolyear     TEXT
+    ,is_kela        BOOLEAN
+    ,is_new         BOOLEAN
+    ,pcs_mean       float
 );
 
-CREATE TABLE IF NOT EXISTS pieces_per_dish (
-    meal_id     INT     NOT NULL
-    ,date       date    NOT NULL
-    ,pcs        float   not null
+CREATE TABLE IF NOT EXISTS meal_names (
+    meal_id         INT
+    ,meal           TEXT
 
-    ,constraint pk_pieces_per_dish primary key (meal_id, date)
 );
+
+-- CREATE TABLE IF NOT EXISTS pieces_per_dish (
+--     meal_id     INT     NOT NULL
+--     ,restaurant TEXT    not null
+--     ,date       date    NOT NULL
+--     ,pcs        float   not null
+
+--     ,constraint pk_pieces_per_dish primary key (meal_id, date, restaurant)
+-- );
 
 
 CREATE TABLE IF NOT EXISTS pieces_whole (
@@ -37,16 +48,13 @@ CREATE TABLE IF NOT EXISTS co2 (
 CREATE TABLE IF NOT EXISTS menu (
     date                    DATE    NOT NULL
     ,restaurant             TEXT    not null
-    ,dish_1                 INT     NOT NULL
-    ,dish_2                 INT     NOT NULL
-    ,dish_3                 INT     NOT NULL
-    ,dish_4                 INT     NOT NULL
-    ,total_co2              float   not null
-    ,total_waste            float   not null
-    ,total_pcs_from_dishes  float   not null
-    ,co2_per_customer       float   not null
-    ,waste_per_customer     float   not null
+    ,meal_ids               INT[]   NOT NULL
+    -- ,total_co2              float   not null
+    -- ,total_waste            float   not null
+    -- ,total_pcs_from_dishes  float   not null
+    -- ,co2_per_customer       float   not null
+    -- ,waste_per_customer     float   not null
     ,fitness                float   not null
 
-    ,constraint pk_menu primary key (date, restaurant)
+    -- ,constraint pk_menu primary key (date, restaurant)
 );
