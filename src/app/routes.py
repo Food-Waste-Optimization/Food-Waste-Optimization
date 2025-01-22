14618e9af65ce4aea21af27f70c5759a6e802d4d
Hoang Le <tommyle133@gmail.com>
2025-01-22 10:08:47 +0700

    Small fix with poetry and post-process

--- src/app/routes.py
+++ src/app/routes.py
@@ -86,8 +86,8 @@ def forecast_receipt():
             df = df.merge(co2, on="meal_id", how="left").merge(
                 biowaste, on="meal_id", how="left"
             )
-            df["co2"].fillna(0.4, inplace=True)
-            df["waste"].fillna(0.01, inplace=True)
+            df["co2"] = df["co2"].fillna(0.4)
+            df["waste"] = df["waste"].fillna(0.01)
 
             out = {"meals": df.to_dict(orient="records"), "whole": pcs_whole}
 
